0982c4e72989e5eca542fc5790562089097402dd
tahapek5454 <tahapek5454@gmail.com>
2024-01-22 13:26:23 +0300

    feat: add common RequestDto

---
+++ Integration/Restaurant.Integration.Domain/Dtos/RequestDto.cs
@@ -0,0 +1,17 @@
+﻿using Restaurant.Integration.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Integration.Domain.Dtos
+{
+    public class RequestDto<T> where T : class
+    {
+        public ActionType ActionType { get; set; } = ActionType.GET;
+        public string Url { get; set; }
+        public T Data { get; set; }
+        public string? AccessToken { get; set; }
+    }
+}
