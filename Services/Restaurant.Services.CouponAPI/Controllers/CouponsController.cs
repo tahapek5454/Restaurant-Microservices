f8b02294d6371f91e88a10ab0992fafd143a7753
tahapek5454 <tahapek5454@gmail.com>
2024-01-22 13:18:51 +0300

    build: build mvc arch

---
+++ Services/Restaurant.Services.CouponAPI/Controllers/CouponsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Restaurant.Integration.Domain.Dtos;
+using Restaurant.Services.CouponAPI.Data.Contexts;
+using Restaurant.Services.CouponAPI.Dtos;
+using Restaurant.Services.CouponAPI.Mapper;
+
+namespace Restaurant.Services.CouponAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CouponsController(AppDbContext _dbContext) : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var datas = await _dbContext.Coupons.ToListAsync();
+
+            var dtos = ObjectMapper.Mapper.Map<List<CouponDto>>(datas);
+
+            var response = ResponseDto<List<CouponDto>>.Sucess(dtos, 200);
+
+            return Ok(response);
+        }
+    }
+}
