2ab2655968f7392866fd854d8639355d6ccaeeb6
tahapek5454 <tahapek5454@gmail.com>
2024-01-22 11:56:42 +0300

    feat: add common responses

---
+++ Integration/Restaurant.Integration.Domain/Dtos/ErrorDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Restaurant.Integration.Domain.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; }
+        public bool IsShow { get; set; }
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+        public ErrorDto(string error, bool isShow) : this()
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+}
