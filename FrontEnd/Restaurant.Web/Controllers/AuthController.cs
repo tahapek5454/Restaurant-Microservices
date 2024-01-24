c677b297a11e8aa693ff78cf80920671384346a6
tahapek5454 <tahapek5454@gmail.com>
2024-01-24 14:25:51 +0300

    feat : add nav link to navbar

---
+++ FrontEnd/Restaurant.Web/Controllers/AuthController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Restaurant.Web.Models.Dtos.Auths;
+using Restaurant.Web.Services.Abstract;
+
+namespace Restaurant.Web.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IAuthService _authService;
+
+        public AuthController(IAuthService authService)
+        {
+            _authService = authService;
+        }
+
+        [HttpGet]
+        public IActionResult Login()
+        {
+            LoginRequestDto loginRequestDto = new LoginRequestDto();
+
+            return View(loginRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            RegistrationRequestDto registrationRequestDto = new RegistrationRequestDto();
+
+            return View(registrationRequestDto);
+        }
+
+        [HttpGet]
+        public IActionResult AssignRoleToUser()
+        {
+           
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Logout()
+        {
+
+
+            return View();
+        }
+    }
+}
