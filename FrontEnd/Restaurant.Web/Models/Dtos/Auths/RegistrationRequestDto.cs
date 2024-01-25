59ac9834cbf55e1b0db1909b6b9e2ab7cb5a3e18
tahapek5454 <tahapek5454@gmail.com>
2024-01-25 14:16:23 +0300

    feat: add basic validation

--- FrontEnd/Restaurant.Web/Models/Dtos/Auths/RegistrationRequestDto.cs
+++ FrontEnd/Restaurant.Web/Models/Dtos/Auths/RegistrationRequestDto.cs
@@ -1,14 +1,31 @@
-﻿namespace Restaurant.Web.Models.Dtos.Auths
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Restaurant.Web.Models.Dtos.Auths
 {
     public class RegistrationRequestDto
     {
+        [Required]
         public string UserName { get; set; }
+        [Required]
+
         public string Name { get; set; }
+        [Required]
+
         public string Surname { get; set; }
+        [Required]
+
         public string Email { get; set; }
+        [Required]
+
         public string PhoneNumber { get; set; }
+        [Required]
+
         public string Password { get; set; }
+        [Required]
+
         public string RePassword { get; set; }
+        [Required]
+
         public string Role { get; set; }
     }
 }
