197a6a93b83555b4debeb639b821c201ef06e484
tahapek5454 <tahapek5454@gmail.com>
2024-01-25 16:17:09 +0300

    feat: add model and dtos for product

---
+++ Services/Restaurant.Services.ProductAPI/Models/Dtos/ProductDto.cs
@@ -0,0 +1,15 @@
+﻿namespace Restaurant.Services.ProductAPI.Models.Dtos
+{
+    public class ProductDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Descripton { get; set; }
+        public double Price { get; set; }
+        public string CategoryName { get; set; }
+        public string? ImageUrl { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public virtual DateTime UpdatedDate { get; set; }
+
+    }
+}
---
+++ Services/Restaurant.Services.ProductAPI/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using Restaurant.Integration.Domain.Entities;
+
+namespace Restaurant.Services.ProductAPI.Models
+{
+    public class Product: BaseEntity
+    {
+        // Default language is English
+        public string? Name_TR { get; set; }
+        public string Name_EN { get; set; }
+        public string? Description_TR { get; set; }
+        public string Descripton_EN { get; set; }
+        public double Price { get; set; }
+        public string? CategoryName_TR { get; set; }
+        public string CategoryName_EN { get; set; }
+        public string? ImageUrl { get; set; }
+
+    }
+}
